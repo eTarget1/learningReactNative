1cbf09ce7a8580dc6b2b86c4126f7f0a9dacc406
eTarget1 <louisclifford10@yahoo.fr>
2019-08-20 13:02:35 -0400

    added Bnanas compoment

--- App.js
+++ App.js
@@ -115,12 +115,14 @@
 
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Bananas from './Bananas';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello, world!</Text>
+        <Bananas />
       </View>
     );
   }
